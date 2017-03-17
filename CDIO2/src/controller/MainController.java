22cb91543c408b9072339f89923d8f70860257f4
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-03-17 08:49:08 +0100

    Comment on R204

--- CDIO2/src/controller/MainController.java
+++ CDIO2/src/controller/MainController.java
@@ -75,9 +75,9 @@ public class MainController implements IMainController, ISocketObserver, IWeight
 		case Q:
 			quit();
 			break;
-		case RM204:
+		case RM204: //Expects an integer reply and does not have to be implemented
 			break;
-		case RM208:
+		case RM208: //Expects a string as a reply
 			break;
 		case S:
 			total = weight - tarWeight;
